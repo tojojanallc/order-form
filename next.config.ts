0cc89872ab0bf50525a60e4fd7a15616e1964ef0
Jonah Levenberg <tojojanallc@gmail.com>
2026-01-26 10:51:04 -0600

    Restoring all stable config files from yesterday

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+module.exports = nextConfig
