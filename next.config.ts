fb6265f10f37e9a77e89b6f467cf638b3b9be658
Jonah Levenberg <tojojanallc@gmail.com>
2026-01-26 10:52:57 -0600

    Force deploy stable Stripe version

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-module.exports = nextConfig
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
