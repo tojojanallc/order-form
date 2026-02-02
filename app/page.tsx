be68c26b2df49e4d3ca132c7bc1f5fb1f883d41e
Jonah Levenberg <tojojanallc@gmail.com>
2026-02-02 15:14:41 -0600

    add size options

--- app/page.tsx
+++ app/page.tsx
@@ -672,23 +672,5 @@ export default function OrderForm() {
         )}
       </div>
     </div>
-
-    {/* DYNAMIC VISUALIZER */}
-<div className="col-span-1">
-    {(() => {
-        // Find the full logo object for the currently selected design
-        const currentLogoObj = mainOptions.find(o => o.label === selectedMainDesign);
-        
-        // Use the database value (default to 'large' if missing)
-        const sizeFromDB = currentLogoObj?.placement || 'large';
-        
-        return (
-            <PlacementVisualizer 
-                garmentType={selectedProduct.type || 'top'} 
-                logoSize={sizeFromDB} // <--- REAL DATA
-            />
-        );
-    })()}
-</div>
   );
 }
