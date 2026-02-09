2ad122bea9d7f864ceca1cf759b8f4306d688267
Jonah Levenberg <tojojanallc@gmail.com>
2026-02-09 14:28:12 -0600

    Fix typescript error in retail order

--- app/api/create-retail-order/route.ts
+++ app/api/create-retail-order/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY // Service Role needed for inventory updates
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY! // Service Role needed for inventory updates
 );
 
 export async function POST(req) {
